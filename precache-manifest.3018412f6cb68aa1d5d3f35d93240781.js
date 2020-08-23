754da73e8c1d260cb638855468dd64f758ab7dc1
Hitesh Paryani <hitesh.paryani@justdial.com>
2020-08-23 21:37:27 +0530

    Updates

---
+++ precache-manifest.3018412f6cb68aa1d5d3f35d93240781.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83bdf8f365825a50f3c948955bde302f",
+    "url": "/diamond-sweeper/index.html"
+  },
+  {
+    "revision": "7e85a676652a7979b144",
+    "url": "/diamond-sweeper/static/css/main.a5407f44.chunk.css"
+  },
+  {
+    "revision": "711c9b0e20cee18a4a79",
+    "url": "/diamond-sweeper/static/js/2.d7ad15c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/diamond-sweeper/static/js/2.d7ad15c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e85a676652a7979b144",
+    "url": "/diamond-sweeper/static/js/main.dacdd523.chunk.js"
+  },
+  {
+    "revision": "a55042564b00510ffd80",
+    "url": "/diamond-sweeper/static/js/runtime-main.d7fb22a7.js"
+  },
+  {
+    "revision": "b44733eb8a5ab06e7e3a51274652614f",
+    "url": "/diamond-sweeper/static/media/dice.b44733eb.gif"
+  }
+]);
